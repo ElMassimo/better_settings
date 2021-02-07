dd2290a8be6b9c0efd4a36817200ae43746bb8bc
Maximo Mussini <maximomussini@gmail.com>
2021-02-07 08:57:26 -0300

    Avoid requiring pry on older rubies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,4 @@ SimpleCov.start { add_filter '/spec/' }
 
 require 'better_settings'
 require 'rspec/given'
-require 'pry-byebug'
+require 'pry-byebug' if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.5.0')
